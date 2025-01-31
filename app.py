2d936150cdc12836badd412b94e4e0dadd435498
anushka <patidaranushka.06@gmail.com>
2025-01-31 20:09:30 +0530

    update final code

---
+++ app.py
@@ -0,0 +1,30 @@
+
+
+from flask import Flask, request, jsonify, render_template
+import joblib
+
+
+app = Flask(__name__)
+
+# Load your trained model and vectorizer
+model = joblib.load('models/logistic_regression_model.pkl')
+vectorizer = joblib.load('models/tfidf_vectorizer.pkl')
+# Home route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Prediction route
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    text = data['text']
+ 
+    # text = preprocess_text(text)
+    vectorized_text = vectorizer.transform([text])
+    prediction = model.predict(vectorized_text)[0]
+    confidence = max(model.predict_proba(vectorized_text)[0]) * 100
+    return jsonify({'sentiment': 'positive' if prediction == 'positive' else 'negative', 'confidence': round(confidence, 2)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
